6d22cc47174139b56fad7d94696b08d9830a7ea4
Alexey Kotlyarov <a@koterpillar.com>
2015-05-25 20:08:49 +1000

    Print scenario/step names on failure

--- lettuce_webdriver/tests/__init__.py
+++ lettuce_webdriver/tests/__init__.py
@@ -27,6 +27,14 @@ def print_source(step):
     try:
         yield
     except:
+        try:
+            step_container = step.scenario
+        except AttributeError:
+            step_container = step.background
+
+        print(step_container.feature.name)
+        print(step_container.name)
+        print(step.sentence)
         print(world.browser.page_source)
         print(world.browser.get_screenshot_as_base64())
         raise
