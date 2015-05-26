b4250eb5cde4869907e65610dc06783ddc4eb22f
Alexey Kotlyarov <a@koterpillar.com>
2015-05-26 21:14:15 +1000

    Remove debug print

--- lettuce_webdriver/tests/__init__.py
+++ lettuce_webdriver/tests/__init__.py
@@ -24,22 +24,3 @@ def with_browser():
 @before.each_feature
 def reset_page(feature):
     world.browser.get('')
-
-
-@around.each_step
-@contextmanager
-def print_source(step):
-    try:
-        yield
-    except:
-        try:
-            step_container = step.scenario
-        except AttributeError:
-            step_container = step.background
-
-        print(step_container.feature.name)
-        print(step_container.name)
-        print(step.sentence)
-        print(world.browser.page_source)
-        print(world.browser.get_screenshot_as_base64())
-        raise
