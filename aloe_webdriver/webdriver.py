d75f19d44ade82e2ae6644109f77a8a8fd617efb
Danielle Madeley <dmadeley@infoxchange.org>
2015-09-04 17:17:22 +1000

    Review fixes

--- aloe_webdriver/webdriver.py
+++ aloe_webdriver/webdriver.py
@@ -4,5 +4,10 @@ Compatibility file to prevent API breakage
 
 from __future__ import absolute_import
 
+import warnings
+
+warnings.warn("This module is deprecated. Import aloe_webdriver directly.",
+              DeprecationWarning)
+
 # pylint:disable=wildcard-import, unused-import, redefined-builtin
 from . import *
