6a32cadf4cf0bf0d031cb856429fc8ce5277961f
Alexey Kotlyarov <a@koterpillar.com>
2015-10-26 20:45:54 +1100

    Move imports and setting up browser up in test_util

--- aloe_webdriver/tests/test_util.py
+++ aloe_webdriver/tests/test_util.py
@@ -12,46 +12,53 @@ from selenium import webdriver
 
 from aloe import world
 from aloe_webdriver.tests.base import PAGES
+from aloe_webdriver.util import (
+    find_button,
+    find_field,
+    find_field_by_id,
+    find_field_by_label,
+    find_field_by_name,
+    option_in_select,
+    wait_for,
+)
 
 # pylint:disable=missing-docstring
 
 
 class TestUtil(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         world.browser = webdriver.Firefox()
+
+    def setUp(self):
         world.browser.get(PAGES['basic_page'])
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         world.browser.quit()
         delattr(world, 'browser')
 
     def test_find_by_id(self):
-        from aloe_webdriver.util import find_field_by_id
         assert find_field_by_id(world.browser, 'password', 'pass')
 
     def test_find_by_name(self):
-        from aloe_webdriver.util import find_field_by_name
         assert find_field_by_name(world.browser, 'submit', 'submit')
         assert find_field_by_name(world.browser, 'select', 'car_choice')
         assert find_field_by_name(world.browser, 'textarea', 'bio')
 
     def test_find_by_label(self):
-        from aloe_webdriver.util import find_field_by_label
         assert find_field_by_label(world.browser, 'text', 'Username:')
 
     def test_no_label(self):
-        from aloe_webdriver.util import find_field_by_label
         assert not find_field_by_label(world.browser, 'text', 'NoSuchLabel')
 
     def test_find_field(self):
-        from aloe_webdriver.util import find_field
         assert find_field(world.browser, 'text', 'username')
         assert find_field(world.browser, 'text', 'Username:')
         assert find_field(world.browser, 'text', 'user')
         assert find_field(world.browser, 'text', 'ชื่อ:')
 
     def test_find_button(self):
-        from aloe_webdriver.util import find_button
         assert find_button(world.browser, 'submit')
         assert find_button(world.browser, 'Submit!')
         assert find_button(world.browser, 'submit_tentative')
@@ -59,13 +66,10 @@ class TestUtil(unittest.TestCase):
         assert find_button(world.browser, 'ส่งฟอร์ม')
 
     def test_option_in_select(self):
-        from aloe_webdriver.util import option_in_select
         assert option_in_select(world.browser, 'Favorite Colors:', 'Blue')
         assert option_in_select(world.browser, 'Favorite Colors:', 'ฟ้า')
 
     def test_wait_for(self):
-        from aloe_webdriver.util import wait_for
-
         counter = [0]
 
         @wait_for
